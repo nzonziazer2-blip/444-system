d8aade63f483989372eb2955db3a0a916232fd30
nzonziazer2-blip <nzonziazer2@gmail.com>
2026-03-04 20:24:56 -0500

    Update setlevel.js

--- src/commands/utility/setlevel.js
+++ src/commands/utility/setlevel.js
@@ -1,25 +1,28 @@
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { PermissionFlagsBits } from 'discord.js';
 import User from '../../models/User.js';
-import { successEmbed } from '../../utils/helpers.js';
+import { successEmbed, errorEmbed } from '../../utils/helpers.js';
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('setlevel')
-    .setDescription('Set level of a member')
-    .addUserOption(opt => opt.setName('user').setDescription('User').setRequired(true))
-    .addIntegerOption(opt => opt.setName('level').setDescription('Level').setRequired(true).setMinValue(0))
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  name: 'setlevel',
+  description: 'Set level of a member',
+  usage: '²setlevel @user [level]',
 
-  async execute(interaction) {
-    const target = interaction.options.getUser('user');
-    const level = interaction.options.getInteger('level');
+  async execute(message, args, client) {
+    if (!message.member.permissions.has(PermissionFlagsBits.Administrator))
+      return message.reply({ embeds: [errorEmbed('Error', 'You need Administrator permission!')] });
+
+    const target = message.mentions.users.first();
+    if (!target) return message.reply({ embeds: [errorEmbed('Error', 'Please mention a user!')] });
+
+    const level = parseInt(args[1]);
+    if (isNaN(level) || level < 0) return message.reply({ embeds: [errorEmbed('Error', 'Please provide a valid level!')] });
 
     await User.findOneAndUpdate(
-      { userId: target.id, guildId: interaction.guild.id },
+      { userId: target.id, guildId: message.guild.id },
       { $set: { level, xp: 0 } },
       { upsert: true }
     );
 
-    await interaction.reply({ embeds: [successEmbed('Level Set', `${target}'s level has been set to **${level}**!`)] });
+    await message.reply({ embeds: [successEmbed('Level Set', `${target}'s level has been set to **${level}**!`)] });
   }
 };
