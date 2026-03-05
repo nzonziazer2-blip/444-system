56d47d74f7cd4b8a173c9cc888c3e53d5c57902d
nzonziazer2-blip <nzonziazer2@gmail.com>
2026-03-04 20:23:15 -0500

    Update banner.js

--- src/commands/utility/banner.js
+++ src/commands/utility/banner.js
@@ -1,21 +1,18 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { EmbedBuilder } from 'discord.js';
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('banner')
-    .setDescription('Get banner of a user')
-    .addUserOption(opt => opt.setName('user').setDescription('User').setRequired(false)),
+  name: 'banner',
+  description: 'Get banner of a user',
+  usage: '²banner @user',
 
-  async execute(interaction) {
-    await interaction.deferReply();
-    const target = await interaction.client.users.fetch(
-      interaction.options.getUser('user')?.id || interaction.user.id,
+  async execute(message, args, client) {
+    const target = await message.client.users.fetch(
+      message.mentions.users.first()?.id || message.author.id,
       { force: true }
     );
 
-    if (!target.banner) {
-      return interaction.editReply({ content: `❌ **${target.username}** has no banner.` });
-    }
+    if (!target.banner)
+      return message.reply({ content: `❌ **${target.username}** has no banner.` });
 
     const embed = new EmbedBuilder()
       .setColor(0x3498db)
@@ -23,6 +20,6 @@ export default {
       .setImage(target.bannerURL({ dynamic: true, size: 1024 }))
       .setTimestamp();
 
-    await interaction.editReply({ embeds: [embed] });
+    await message.reply({ embeds: [embed] });
   }
 };
