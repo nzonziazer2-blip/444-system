d38467a35eadc669f3b16da8ff3336e768285e8e
nzonziazer2-blip <nzonziazer2@gmail.com>
2026-03-04 20:26:30 -0500

    Update jail.js

--- src/commands/utility/jail.js
+++ src/commands/utility/jail.js
@@ -1,72 +1,57 @@
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { PermissionFlagsBits } from 'discord.js';
 import Guild from '../../models/Guild.js';
 import { successEmbed, errorEmbed } from '../../utils/helpers.js';
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('jail')
-    .setDescription('Jail or unjail a member')
-    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
-    .addSubcommand(sub => sub
-      .setName('set')
-      .setDescription('Setup jail system')
-      .addRoleOption(opt => opt.setName('role').setDescription('Jail role').setRequired(true))
-      .addChannelOption(opt => opt.setName('channel').setDescription('Jail channel').setRequired(true)))
-    .addSubcommand(sub => sub
-      .setName('add')
-      .setDescription('Jail a member')
-      .addUserOption(opt => opt.setName('user').setDescription('User to jail').setRequired(true))
-      .addStringOption(opt => opt.setName('reason').setDescription('Reason').setRequired(false)))
-    .addSubcommand(sub => sub
-      .setName('remove')
-      .setDescription('Unjail a member')
-      .addUserOption(opt => opt.setName('user').setDescription('User to unjail').setRequired(true))),
+  name: 'jail',
+  description: 'Jail or unjail a member',
+  usage: '²jail set @role #channel | ²jail add @user [reason] | ²jail remove @user',
 
-  async execute(interaction) {
-    const sub = interaction.options.getSubcommand();
+  async execute(message, args, client) {
+    if (!message.member.permissions.has(PermissionFlagsBits.ModerateMembers))
+      return message.reply({ embeds: [errorEmbed('Error', 'You don\'t have permission!')] });
+
+    const sub = args[0]?.toLowerCase();
 
     if (sub === 'set') {
-      const role = interaction.options.getRole('role');
-      const channel = interaction.options.getChannel('channel');
+      const role = message.mentions.roles.first();
+      const channel = message.mentions.channels.first();
+
+      if (!role) return message.reply({ embeds: [errorEmbed('Error', 'Please mention a role!')] });
+      if (!channel) return message.reply({ embeds: [errorEmbed('Error', 'Please mention a channel!')] });
 
       await Guild.findOneAndUpdate(
-        { guildId: interaction.guild.id },
+        { guildId: message.guild.id },
         { $set: { jailRole: role.id, jailChannel: channel.id } },
         { upsert: true }
       );
 
-      return interaction.reply({ embeds: [successEmbed('Jail Setup', `Jail role: ${role}\nJail channel: ${channel}`)] });
+      return message.reply({ embeds: [successEmbed('Jail Setup', `Jail role: ${role}\nJail channel: ${channel}`)] });
     }
 
     if (sub === 'add') {
-      const target = interaction.options.getUser('user');
-      const reason = interaction.options.getString('reason') || 'No reason provided';
-      const member = interaction.guild.members.cache.get(target.id);
-      const guildData = await Guild.findOne({ guildId: interaction.guild.id });
+      const target = message.mentions.users.first();
+      const reason = args.slice(2).join(' ') || 'No reason provided';
+      const member = message.guild.members.cache.get(target?.id);
+      const guildData = await Guild.findOne({ guildId: message.guild.id });
 
-      if (!guildData?.jailRole) return interaction.reply({ embeds: [errorEmbed('Error', 'Jail role not set. Use `/jail set` first.')], ephemeral: true });
-      if (!member) return interaction.reply({ embeds: [errorEmbed('Error', 'Member not found.')], ephemeral: true });
-
-      // Save current roles and assign jail role
-      const currentRoles = member.roles.cache
-        .filter(r => r.id !== interaction.guild.id)
-        .map(r => r.id);
+      if (!guildData?.jailRole) return message.reply({ embeds: [errorEmbed('Error', 'Jail role not set. Use `²jail set` first.')] });
+      if (!member) return message.reply({ embeds: [errorEmbed('Error', 'Member not found.')] });
 
       await member.roles.set([guildData.jailRole]);
-
-      return interaction.reply({ embeds: [successEmbed('Member Jailed', `${target} has been jailed.\n**Reason:** ${reason}`)] });
+      return message.reply({ embeds: [successEmbed('Member Jailed', `${target} has been jailed.\n**Reason:** ${reason}`)] });
     }
 
     if (sub === 'remove') {
-      const target = interaction.options.getUser('user');
-      const member = interaction.guild.members.cache.get(target.id);
-      const guildData = await Guild.findOne({ guildId: interaction.guild.id });
+      const target = message.mentions.users.first();
+      const member = message.guild.members.cache.get(target?.id);
+      const guildData = await Guild.findOne({ guildId: message.guild.id });
 
-      if (!guildData?.jailRole) return interaction.reply({ embeds: [errorEmbed('Error', 'Jail role not set.')], ephemeral: true });
-      if (!member) return interaction.reply({ embeds: [errorEmbed('Error', 'Member not found.')], ephemeral: true });
+      if (!guildData?.jailRole) return message.reply({ embeds: [errorEmbed('Error', 'Jail role not set.')] });
+      if (!member) return message.reply({ embeds: [errorEmbed('Error', 'Member not found.')] });
 
       await member.roles.remove(guildData.jailRole);
-      return interaction.reply({ embeds: [successEmbed('Member Unjailed', `${target} has been unjailed.`)] });
+      return message.reply({ embeds: [successEmbed('Member Unjailed', `${target} has been unjailed.`)] });
     }
   }
 };
